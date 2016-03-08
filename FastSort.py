221407510f7454b44a32910dde6ad2fa48e360a8
dreamer2018 <zhoupans_mail@163.com>
2016-03-08 20:24:51 +0800

    快排——递归式

--- FastSort.py
+++ FastSort.py
@@ -16,7 +16,11 @@ def Sort(list,start,end):
         list[i]=k
     return i
 def FastSort(list,start,end):
-    
+    if(start<end):
+        t=Sort(list,start,end)
+        FastSort(list,start,t-1)
+        FastSort(list,t+1,end)
 if __name__ == '__main__':
     list=[5,8,3,6,4,2]
     FastSort(list,0,len(list)-1)
+    print list
