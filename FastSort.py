7762b00b3d17f9085bb363c450002f3eed03829d
dreamer2018 <zhoupans_mail@163.com>
2016-03-08 20:20:33 +0800

    快排

--- FastSort.py
+++ FastSort.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 #!coding=utf-8
 __author__ = 'zhoupan'
+def Sort(list,start,end):
+    #如果元素只有一个，则直接返回
+    i=start
+    j=end
+    k=list[start]
+    while(i<j):
+        while(i<j and list[j] >= k):
+            j-=1
+        list[i]=list[j]
+        while(i<j and list[i] < k):
+            i+=1
+        list[j]=list[i]
+        list[i]=k
+    return i
+def FastSort(list,start,end):
+    
 if __name__ == '__main__':
-    pass
+    list=[5,8,3,6,4,2]
+    FastSort(list,0,len(list)-1)
