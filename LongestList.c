0ea506a280c2cc922bffcfd1e6281a51f5f73b9b
dreamer2018 <zhoupans_mail@163.com>
2016-04-19 22:13:29 +0800

    add

---
+++ LongestList.c
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: LongestList.c
+	> Author: ZhouPan / github: dreamer2018 
+	> Mail: zhoupans_mail@163.com
+	> Created Time: Tue 19 Apr 2016 12:25:37 PM CST
+  > Function:最长公共子序列
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+int countLongestList(char *str,int n)
+{
+    int i,j;
+    int temp[n];
+    memset(temp,0,n*sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+
+        }
+    }
+}
+void main()
+{
+    char str[100];
+    scanf("%s",str);
+}
