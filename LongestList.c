1707b6cc723a8892a3953c96a384d17d24323808
ZhouPan <zhoupan@Lenovo-G485.workgroup>
2016-04-19 23:46:55 +0800

    add

--- LongestList.c
+++ LongestList.c
@@ -8,21 +8,44 @@
 
 #include<stdio.h>
 #include<string.h>
+int Max(int *list,int n)
+{
+    int i,max;
+    max=list[0];
+    for(i=1;i<n;i++)
+    {
+        if(list[i]>max)
+        {
+            max=list[i];
+        }
+    }
+    return max;
+}
 int countLongestList(char *str,int n)
 {
     int i,j;
     int temp[n];
-    memset(temp,0,n*sizeof(int));
+    
+    for(i=0;i<n;i++)
+    {
+        temp[i]=1;
+    }
     for(i=0;i<n;i++)
     {
         for(j=0;j<i;j++)
         {
-
+            if(str[i]>str[j] && temp[i]<temp[j]+1)
+            {
+                temp[i]=temp[j]+1;
+            }
         }
     }
+    return Max(temp,n);
 }
 void main()
 {
     char str[100];
     scanf("%s",str);
+    int result=countLongestList(str,strlen(str));
+    printf("result=%d\n",result);
 }
