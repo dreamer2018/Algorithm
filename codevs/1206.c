0b5fc69b0e4e20642328cb5bb841e05f9310d62c
dreamer2018 <zhoupans_mail@163.com>
2016-04-20 19:31:54 +0800

    add

---
+++ codevs/1206.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: TwoPoint.c
+	> Author: ZhouPan / github: dreamer2018 
+	> Mail: zhoupans_mail@163.com
+	> Created Time: Wed 20 Apr 2016 03:25:53 PM CST
+    > Function:输入浮点数，保留两问后输出
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    double a;
+    scanf("%lf",&a);
+    printf("%.2lf",a);
+    return 0;
+}
