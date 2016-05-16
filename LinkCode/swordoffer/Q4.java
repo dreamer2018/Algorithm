2683fcbb7a137c2eb1ed6f196518320571b51c4c
dreamer2018 <zhoupans_mail@163.com>
2016-05-16 21:27:49 +0800

    add

---
+++ LinkCode/swordoffer/Q4.java
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: Q4.java
+	> Author: ZhouPan / github: dreamer2018
+	> Mail: zhoupans_mail@163.com
+	> Created Time: Mon 16 May 2016 09:13:05 PM CST
+    > Function:
+ ************************************************************************/
+public class Q4{
+    public int replaceBlank(char[] string, int length){
+        // Write your code here
+        for(int i=0;i<=length;i--){
+            if(string[i] == ' '){
+
+            }
+        }
+    }
+    public static void move(char[] string,int index){
+        for(int i=;i<)
+    }
+}
+
