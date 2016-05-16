559030b26c2136eb5af704997c7ede5cbc40124c
dreamer2018 <zhoupans_mail@163.com>
2016-05-16 20:50:49 +0800

    swordoffer

---
+++ LinkCode/swordoffer/Q3.java
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: Q3.java
+	> Author: ZhouPan / github: dreamer2018
+	> Mail: zhoupans_mail@163.com
+	> Created Time: Mon 16 May 2016 12:07:04 AM
+    > Function:
+ ************************************************************************/
+public class Q3{
+    public static Solution getInstance() {
+        // write your code here
+        private static int sign=0;
+        if(sign == 0){
+            Q3 s=new Q3();
+        }
+        sign++;
+        return s;
+    }
+}
+
