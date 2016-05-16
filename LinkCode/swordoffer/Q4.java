553384f5f9862b1f4abd42d4d2f3ee3ee6f2db04
dreamer2018 <zhoupans_mail@163.com>
2016-05-16 22:47:12 +0800

    Q4

--- LinkCode/swordoffer/Q4.java
+++ LinkCode/swordoffer/Q4.java
@@ -8,14 +8,34 @@
 public class Q4{
     public int replaceBlank(char[] string, int length){
         // Write your code here
-        for(int i=0;i<=length;i--){
+        for(int i=0;i<=length;i++){
             if(string[i] == ' '){
-
+                moveAndreplaceString(string,index,length);
+                length+=2;
             }
         }
+        return 0;
     }
-    public static void move(char[] string,int index){
-        for(int i=;i<)
+    public void moveAndreplaceString(char[] string,int index,int length){
+        for(int i=length-1;i>index;i--){
+            string[i+2]=string[i];
+        }
+        string[index]='%';
+        string[index+1]='2';
+        string[index+2]='0';
+    }
+    public static void main(String[] args){
+        char[] string = new char[100];
+        string[0]='h';
+        string[1]=' ';
+        string[2]='e';
+        string[3]=' ';
+        string[4]='l';
+        string[5]=' ';
+        string[6]='l';
+        string[7]=' ';
+        string[8]='o';
+        new Q4().replaceBlank(string,length);
     }
 }
 
