e7232d6e09ef0d9437e6a184c40364452ed35f2a
dreamer2018 <zhoupans_mail@163.com>
2016-05-16 21:12:25 +0800

    Q3

--- LinkCode/swordoffer/Q3.java
+++ LinkCode/swordoffer/Q3.java
@@ -3,17 +3,22 @@
 	> Author: ZhouPan / github: dreamer2018
 	> Mail: zhoupans_mail@163.com
 	> Created Time: Mon 16 May 2016 12:07:04 AM
-    > Function:
+    > Function:单例问题
  ************************************************************************/
 public class Q3{
-    public static Solution getInstance() {
+    public static Q3 instance= null;
+    public static Q3 getInstance() {
         // write your code here
-        private static int sign=0;
-        if(sign == 0){
-            Q3 s=new Q3();
+        return instance == null ? instance : new Q3();
+    }
+    public static void main(String[] args){
+        Q3 q1=Q3.getInstance();
+        Q3 q2=Q3.getInstance();
+        if(q1 == q2){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
         }
-        sign++;
-        return s;
     }
 }
 
