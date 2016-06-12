2a3e29297e600be62992d865903ef80d1d23194e
dreamer2018 <zhoupans_mail@163.com>
2016-06-12 09:07:44 +0800

    整数变换

--- change.c
+++ change.c
@@ -7,27 +7,29 @@
  ************************************************************************/
 
 #include<stdio.h>
+#define N 100
 
-int k;
+int count=0;  //统计需要变换的数量
+int x[N];   //记录变换的过程
 
-int ChangeFun(int n,int *h,int Sign)
+int ChangeFun(int n,int *h,int Sign)  //根据Sign的值，变换计算方法，h用于记录使用了那种方法
 {
-    if(Sign)
+    if(!Sign)
     {
-        *h=1;
+        *h=0;
         return 3*n;
     }
     else
     {
-        *h=2;
+        *h=1;
         return n/2;
     }
 }
 
-int SerachWay(int dept,int n,int m)
+int SerachWay(int dept,int k,int n,int m)
 {
     int num;
-    if(dept > k)
+    if(dept > k) //当
     {
         return 0;
     }
@@ -36,16 +38,35 @@ int SerachWay(int dept,int n,int m)
     {
         int tmp;
         num=ChangeFun(n,&tmp,i);
-        if(num == m && SerachWay(dept+1,num,m))
+        if(num == m || SerachWay(dept+1,k,num,m))
         {
-            printf("%d\t",tmp);
+            count++;
+            x[count-1]=tmp;
             return 1;
         }
     }
     return 0;
 }
+void Print()
+{
+    int i;
+    printf("%d\n",count);
+    for(i=0;i<count;i++)
+    {
+        printf("%c",x[i]? 'g':'f');
+    }
+    printf("\n");
+}
 
 int main()
 {
+    int m,n;
+    int k=1;
+    scanf("%d %d",&n,&m);
+    while(!SerachWay(1,k,n,m))
+    {
+        k++;
+    }
+    Print();
     return 0;
 }
